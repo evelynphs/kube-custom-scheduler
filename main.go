91fae1e8a27e75be56c9c0d61cca8e83db0ecc4a
evelynphs <evelyn.p.h.silalahi@gmail.com>
2026-02-16 17:14:00 +0700

    edit main.go

--- main.go
+++ main.go
@@ -8,14 +8,13 @@ import (
     "k8s.io/kubernetes/cmd/kube-scheduler/app"
 
     // Import your custom plugins
-    "github.com/evelynphs/custom-scheduler/plugins"
+    "github.com/evelynphs/kube-custom-scheduler/plugins"
 )
 
 func main() {
     // Register custom plugins with the scheduler framework
     command := app.NewSchedulerCommand(
         app.WithPlugin(plugins.GPUAwareName, plugins.NewGPUAwarePlugin),
-        app.WithPlugin(plugins.CostOptimizedName, plugins.NewCostOptimizedPlugin),
     )
 
     code := cli.Run(command)
