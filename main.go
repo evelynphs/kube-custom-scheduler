616e71ca298d6c1bb16640221c53514a88320a94
evelynphs <evelyn.p.h.silalahi@gmail.com>
2026-02-22 15:27:24 +0700

    change package in main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	command := scheduler.NewSchedulerCommand(
-		scheduler.WithPlugin(plugin.Name, plugin.New),
+		scheduler.WithPlugin(edf.Name, edf.New),
 	)
 	if err := command.Execute(); err != nil {
 		klog.Fatal(err)
